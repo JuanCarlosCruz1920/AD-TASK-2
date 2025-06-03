1fbf5876cfe8389b373e9dffe38ec87b37df8f7c
Juan Carlos S. Cruz <202310023@fit.edu.ph>
2025-06-03 15:53:44 +0800

    Delete AD-Task-2 directory

--- AD-Task-2/components/footer.component.php
+++
@@ -1,8 +0,0 @@
-</main>
-    <footer class="footer">
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> NeoScreen. All rights reserved.</p>
-        </div>
-    </footer>
-</body>
-</html>
--- AD-Task-2/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('BASE_PATH', __DIR__);
-
-require_once BASE_PATH . '/bootstrap.php';
-
-require_once BASE_PATH . '/router.php';
